9d13966cd81346c5dfd8d73f8a7bf4be37bbfddc
mohan-p <mohanpreddy2003@gmail.com>
2026-05-05 12:14:26 +0530

    Fix wildcard route syntax for Render deployment

--- backend/server-production.js
+++ backend/server-production.js
@@ -34,7 +34,7 @@ app.get('/test/followups', async (req, res) => {
 });
 
 // Serve React app for all other routes
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
