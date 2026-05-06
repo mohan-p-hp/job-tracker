71feaa8f26dd1924b79fe4c12e3cc6bad3c5f989
mohan-p <mohanpreddy2003@gmail.com>
2026-05-06 21:27:57 +0530

    Use regex pattern /.*/ for catch-all route compatibility

--- backend/server-production.js
+++ backend/server-production.js
@@ -53,7 +53,7 @@ app.get('/test/todo-reminders', async (req, res) => {
 // app.get('(.*)', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 // });
-app.get('/*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 app.listen(PORT, () => {
